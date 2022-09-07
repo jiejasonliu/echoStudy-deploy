f2c7711a3db83603f9060b69fb0cbc274f9ec7e8
jiejasonliu <jasonningliu@gmail.com>
2022-09-07 01:25:55 -0600

    feat: remove --legacy from deploy script
    package peer dependency issues with @craco/craco@7.0.0-alpha.* has been resolved as of craco-esbuild@0.5.2

--- deploy.ps1
+++ deploy.ps1
@@ -5,4 +5,4 @@ $filepath = "./deploy-logs/deploy-$time.log"
 ### run script without stdout buffer and redirect stderr to stdout
 ### pipe result ToString to avoid errors propagating to the terminal
 ### pipe to tee to write log file while still logging results
-py -u trydeploy.py --legacy 2>&1 | % ToString | tee $filepath
+py -u trydeploy.py 2>&1 | % ToString | tee $filepath
